5906de5d18f48384ffb85fe09fc809c1911cb638
David Liu <77701856+daichungus@users.noreply.github.com>
2025-12-05 16:37:01 -0500

    Revamped testing shell

--- run_test.sh
+++ run_test.sh
@@ -2,13 +2,24 @@
 make clean
 make
 
-printf "START: " | tee -a log.txt
-date | tee -a log.txt
+LOG_DIR="./logs"
+mkdir -p "$LOG_DIR"
 
-./edist | tee -a log.txt
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+LOG_FILE="$LOG_DIR/log_$TIMESTAMP.txt"
 
-printf "END: " | tee -a log.txt
-date | tee -a log.txt
-echo | tee -a log.txt
+printf "START: " | tee -a "$LOG_FILE"
+date | tee -a "$LOG_FILE"
+echo | tee -a "$LOG_FILE"
+
+N="3"
+for i in $(seq 1 $N)
+do
+	./edist | tee -a "$LOG_FILE"
+done 
+
+printf "END: " | tee -a "$LOG_FILE"
+date | tee -a "$LOG_FILE"
+echo | tee -a "$LOG_FILE"
 
 make clean
