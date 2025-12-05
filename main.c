c52962cd51b26dbfe4578840ff1cdf0169a4807c
David Liu <77701856+daichungus@users.noreply.github.com>
2025-12-05 16:37:17 -0500

    Limit run to 1 test only

--- main.c
+++ main.c
@@ -12,19 +12,22 @@ static inline double now_ms(void) {
 }
 
 int main() {
-    
-    printf("Small test cases:\n");
+
     double t0 = now_ms();
-    run_tests_small(); 
-    double t1 = now_ms();
-    printf("Elapsed time: %.6f ms\n", t1 - t0);
-    printf("\n");
-    
-    printf("1 million-length string test:\n");
-    double t2 = now_ms();
     run_test_1mil(); 
-    double t3 = now_ms();
-    printf("Elapsed time: %.6f ms\n", t3 - t2);
+    double t1 = now_ms();
+    double elapsed_time = t1 - t0;
+
+    // Determine elapsed time units
+    if (elapsed_time > 3.6e+6) {
+        printf("Elapsed time: %.6f hr\n", elapsed_time / 3.6e+6);
+    } else if (elapsed_time > 60000.0) {
+        printf("Elapsed time: %.6f min\n", elapsed_time / 60000.0);
+    } else if (elapsed_time > 1000.0) {
+        printf("Elapsed time: %.6f s\n", elapsed_time / 1000.0);
+    } else {
+        printf("Elapsed time: %.6f ms\n", elapsed_time);
+    }
     printf("\n");
 
     return 0;
